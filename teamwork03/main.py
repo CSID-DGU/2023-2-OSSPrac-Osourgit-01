317f0a1a472c9bf03c05011786b639bd9e8f221c
parksuhhyun <2022111511@dgu.ac.kr>
2023-10-31 21:48:24 +0900

    박서현 programming languages

--- teamwork03/main.py
+++ teamwork03/main.py
@@ -1,27 +1,19 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def input():
     return render_template('input.html')
 
-@app.route('/result',methods=['POST','GET'])
+@app.route('/result', methods=['POST', 'GET'])
 def result():
-    if request.method =='POST':
-        result=dict()
-        result['Name']=request.form.get('name')
-        result['Student Number']=request.form.get('StudentNumber')
+    if request.method == 'POST':
+        result = dict()
+        result['Name'] = request.form.get('name')
+        result['Student_Number'] = request.form.get('StudentNumber')
+        result['Programming_Languages'] = request.form.getlist('programming_languages')
+        return render_template('result.html', result=result)
 
-        # Gender Selection
-
-        # Major
-
-        # Programming Languages
-
-
-        return render_template('result.html',result=result)
-
-
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
