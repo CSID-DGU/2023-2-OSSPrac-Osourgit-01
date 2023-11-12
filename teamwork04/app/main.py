37034667c049bbcd6cff5b204be4fb06e2863767
ParkSuhhyun <2022111511@dgu.ackr>
2023-11-12 21:32:47 +0900

    코드 수정

--- teamwork04/app/main.py
+++ teamwork04/app/main.py
@@ -1,29 +1,26 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def input():
     return render_template('input.html')
 
-@app.route('/result',methods=['POST','GET'])
+@app.route('/result', methods=['POST', 'GET'])
 def result():
-    if request.method =='POST':
-        result=dict()
-        result['Name']=request.form.get('name')
-        result['Student Number']=request.form.get('StudentNumber')
-
-        # Gender Selection
-        result['Gender']=request.form.get('gender')
-        
-        # Major
-        result['Major']=request.form.get('major')
-
-        # Programming Languages
+    if request.method == 'POST':
+        result = dict()
+        result['Name'] = request.form.get('name')
+        result['Student Number'] = request.form.get('StudentNumber')
+        result['Gender'] = request.form.get('gender')
+        result['Major'] = request.form.get('major')
+        email_id = request.form.get('email_id')
+        email_domain = request.form.get('email_domain')
+        result['Email'] = f"{email_id}@{email_domain}"
         result['Programming Languages'] = ', '.join(request.form.getlist('programming_languages[]'))
 
-        return render_template('result.html',result=result)
+        return render_template('result.html', result=result)
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
